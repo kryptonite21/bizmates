7035d9d5572f42bbed6e37f475b7b5358f5e3e34
Robin <rjcor21@gmail.com>
2019-05-19 18:40:28 +0800

    Edited the route urls

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,15 +8,15 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Bizmates Weather</a>
+          <a class="navbar-brand" href="{{ url('/') }}">Bizmates Weather</a>
         </div>
     
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact Us</a></li>
-            <li><a href="#">FAQs</a></li>
+            <li class="active"><a href="{{ url('/') }}">Home</a></li>
+            <li><a href="{{ url('about') }}">About</a></li>
+            <li><a href="{{ url('contact') }}">Contact Us</a></li>
+            <li><a href="{{ url('faqs') }}">FAQs</a></li>
           </ul>
         </div>
       </div>
