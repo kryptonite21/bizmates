aeef1338054774b2121230161e39c45dde0ef7dd
Robin <rjcor21@gmail.com>
2019-05-19 17:40:18 +0800

    Edited the templates.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,9 +8,9 @@
     <link rel="stylesheet" href="{{ asset('startbootstrap/css/bootstrap.min.css') }}">
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="{{ asset('jquery/jquery-3.4.0.min.js') }}"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.2/js/bootstrap.min.js"></script>
+    <script src="{{ asset('startbootstrap/js/bootstrap.min.js') }}"></script>
 
   </head>
   <body>
